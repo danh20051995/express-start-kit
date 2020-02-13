ee6ecf04b27705dc93ff5acc2bb4fe43e0982d4f
danhdanh <danh.danh20051995@gmail.com>
2020-02-13 15:39:11 +0700

    update pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: 'maua-api',
+      name: 'express-api',
       script: 'npm',
       args: 'run production',
       // watch: false,
       // instances: 'max',
       // exec_mode: 'cluster',
-      // log_file: './pm2logs/maua_outerr.log',
-      // error_file: './pm2logs/maua_error.log',
-      // out_file: './pm2logs/maua_output.log',
+      // log_file: './pm2logs/express_outerr.log',
+      // error_file: './pm2logs/express_error.log',
+      // out_file: './pm2logs/express_output.log',
       env: {
         NODE_ENV: 'production'
       }
